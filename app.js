8d567db433657c93694e7f1ae34f48467c869d86
DonavanGrobler <donavangrobler2@gmail.com>
2024-06-28 16:16:35 +0200

    Changes localhost route

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoPractice = require('./mongo');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoPractice = require("./mongo");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/products', mongoPractice.createProduct);
+app.post("/products", mongoPractice.createProduct);
 
-app.get('/products');
+app.get("/products");
 
-app.listen(3000);
+app.listen(5001);
