c39098f92e37b071e0022bb44d6983e5868fc8b1
DonavanGrobler <donavangrobler2@gmail.com>
2024-06-28 16:09:57 +0200

    first commit

---
+++ mongo.js
@@ -0,0 +1,28 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url =
+  "mongodb+srv://donavangrobler:42069@mern-stack-project.ad4vguk.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Stack-Project";
+
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data." });
+  }
+  client.close();
+
+  res.json(newProduct);
+};
+
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
